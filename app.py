ecfe57660570b1949f42ff39145400c4d323a701
msharina <matthew_sharin@brown.edu>
2024-04-24 10:14:30 -0400

    login page setup

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=["GET"])
+@app.route('/')
 def hello():
     return render_template('home.html')
 
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
