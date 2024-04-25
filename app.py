65a896c555d1e170db14a242c90804186d186b7d
khinzen <krishan_hinzen@brown.edu>
2024-04-25 13:27:19 -0400

    two linked pages
    push a prompt through the website

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["POST", "GET"])
 def hello():
-    return render_template('home.html')
+    if request.method == "POST":
+        prompt = request.form["in"]
+        return render_template('result.html', prompt = prompt)
+    else:
+	    return render_template("home.html")
 
 @app.route('/index')
 def index():
     return render_template('index.html')
-
-@app.route('/result')
-def result():
-    return render_template('result.html')
 
 @app.route('/login')
 def login():
