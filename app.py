706edb9d0b3f1f4be162da520d6c8906dfdc9a81
msharina <matthew_sharin@brown.edu>
2024-05-03 00:05:34 -0400

    continued login work

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template, request, redirect, url_for
 from openai import OpenAI
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, ValidationError
+
 
 from helpers import sent_score, posneg
 
@@ -26,7 +30,23 @@ app.app_context().push()
 class User(db.Model, UserMixin):
      id = db.Column(db.Integer, primary_key=True)
      username = db.Column(db.String(20), nullable=False, unique=True)
-     password = db.Column(db.String(20), nullable=False)
+     password = db.Column(db.String(80), nullable=False)
+
+class RegisterForm(FlaskForm):
+     username = StringField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+     password = PasswordField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+     submit = SubmitField("Register")
+
+     def validate_username(self, username):
+          existing_user_username = User.query.filter_by(username=username.data).first()
+          if existing_user_username:
+               raise ValidationError("Choose a different username, that one already exists.")
+
+class LoginForm(FlaskForm):
+     username = StringField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+     password = PasswordField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder": "Password"})
+     submit = SubmitField("Login")
+
 
 
 
@@ -58,7 +78,13 @@ def index():
 
 @app.route('/login', methods=["POST", "GET"])
 def login():
+    form = LoginForm()
     return render_template('login.html')
+
+@app.route('/register', methods=["POST", "GET"])
+def register():
+    form = RegisterForm()
+    return render_template('register.html')
 
 @app.route('/dashboard', methods=["POST", "GET"])
 def dashboard():
