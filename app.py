8b9abecea7dbfbe1f56d452d625bea70be8156b9
msharina <matthew_sharin@brown.edu>
2024-05-03 01:17:44 -0400

    make login and register pages functional

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for
 from openai import OpenAI
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
-
+from flask_bcrypt import Bcrypt
 
 from helpers import sent_score, posneg, overall_sentiment
 
@@ -15,17 +15,23 @@ api_key = "LL-JsXHgYMuGCr4WmyOeWH5tVD03o57tlJBFXTJG5v2WsnybEoMB01JzbJWkZ8unAg7",
 base_url = "https://api.llama-api.com"
 )
 
-#temporary
-logged_in = True
 
 app = Flask(__name__)
 
 
-
+bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'key'
 db = SQLAlchemy(app)
 app.app_context().push()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+     return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
      id = db.Column(db.Integer, primary_key=True)
@@ -82,16 +88,34 @@ def index():
 @app.route('/login', methods=["POST", "GET"])
 def login():
     form = LoginForm()
-    return render_template('login.html')
+    if form.validate_on_submit():
+         user = User.query.filter_by(username=form.username.data).first()
+         if user:
+              if bcrypt.check_password_hash(user.password, form.password.data):
+                   login_user(user)
+                   return redirect(url_for('dashboard'))
+    return render_template('login.html', form=form)
 
 @app.route('/register', methods=["POST", "GET"])
 def register():
     form = RegisterForm()
-    return render_template('register.html')
+
+    if form.validate_on_submit():
+         hashed_password = bcrypt.generate_password_hash(form.password.data)
+         new_user = User(username=form.username.data, password=hashed_password)
+         db.session.add(new_user)
+         db.session.commit()
+         return redirect(url_for('login'))
+
+    return render_template('register.html', form=form)
 
 @app.route('/dashboard', methods=["POST", "GET"])
+@login_required
 def dashboard():
-    if logged_in == True:
-        return render_template('dashboard.html')
-    else:
-         return render_template('login.html')
+    return render_template('dashboard.html')
+
+@app.route('/logout', methods=["POST", "GET"])
+@login_required
+def logout():
+     logout_user()
+     return redirect(url_for('login'))
