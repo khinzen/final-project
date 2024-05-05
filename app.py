b2711ddc3229b69ff46959152cf3eeedd2b6983d
khinzen <krishan_hinzen@brown.edu>
2024-05-05 10:55:54 -0400

    adding prompt table
    prompt table

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from openai import OpenAI
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
@@ -37,6 +37,11 @@ class User(db.Model, UserMixin):
      id = db.Column(db.Integer, primary_key=True)
      username = db.Column(db.String(20), nullable=False, unique=True)
      password = db.Column(db.String(80), nullable=False)
+
+class Prompts(db.Model):
+     id = db.Column(db.Integer, db.ForeignKey('User.id'), primary_key=True)
+     input = db.Column(db.String(1000), nullable=False, unique=False)
+     output = db.Column(db.String(1000), nullable=False)
 
 class RegisterForm(FlaskForm):
      username = StringField(validators=[InputRequired(),Length(min=4, max=20)], render_kw={"placeholder": "Username"})
@@ -87,6 +92,9 @@ def result():
         stats = sent_score(answer)
         pos, neg = posneg(stats, answer)
         overall = overall_sentiment(stats)
+
+        session['prompt'] = prompt
+        session['answer'] = answer
 
         return render_template('result.html', prompt = prompt, answer = answer, pos = pos, neg = neg, overall = overall)
 
